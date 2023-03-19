7ad33ab9de9914299b77cfaf8e61361de9a398aa
刘健 <liujian@liujiandeMacBook-Pro.local>
2023-03-19 16:16:20 +0800

    add

---
+++ data_type/data_type.h
@@ -0,0 +1,21 @@
+//
+// Created by 刘健 on 2023/3/19.
+//
+
+#ifndef IMOOCCPP_DATA_TYPE_H
+#define IMOOCCPP_DATA_TYPE_H
+#pragma once
+#include <iostream>
+using namespace std;
+namespace data_type {
+
+class Solution {
+public:
+    static void test() {
+        std::string str;
+        cout << sizeof (str) << endl;
+    }
+};
+
+}
+#endif //IMOOCCPP_DATA_TYPE_H
