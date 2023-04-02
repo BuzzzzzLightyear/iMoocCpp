6c95bcb35ee07065c532a6b80b5013f715d6d6d2
BuzzzzzLightyear <104250118+BuzzzzzLightyear@users.noreply.github.com>
2023-04-03 01:32:27 +0800

    fix

---
+++ tianyan_task/tianyan_task2.cpp
@@ -0,0 +1,4 @@
+//
+// Created by 刘健 on 2023/4/3.
+//
+#include "tianyan_task2.h"
---
+++ tianyan_task/tianyan_task2.h
@@ -0,0 +1,8 @@
+//
+// Created by 刘健 on 2023/4/3.
+//
+
+#ifndef IMOOCCPP_TIANYAN_TASK2_H
+#define IMOOCCPP_TIANYAN_TASK2_H
+using namespace std;
+#endif //IMOOCCPP_TIANYAN_TASK2_H
